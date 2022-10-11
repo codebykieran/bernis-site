cea47528e5fef0326942074bef6d5847b58509c6
Kieran <mr.kieran.mcbride@googlemail.com>
2022-10-11 22:11:21 +0100

    Application - Installed nuxt/axios module
    - installed axios module and tested pulling through products information

--- frontend/pages/products.vue
+++ frontend/pages/products.vue
@@ -1,5 +1,32 @@
 <template>
+    <div>
+        <div v-for="product in products" :key=product.id>
+            {{ product.attributes.name }}
+            {{ product.attributes.description }}
+            {{ '£' + product.attributes.price }}
+            <img :src="'http://localhost:1337' + product.attributes.image.data.attributes.formats.medium.url"/> 
+        </div>
+    </div>
 </template>
 
-<script setup>
+<script>
+
+export default {
+
+    data() {
+        return {
+            products: []
+        }
+    },
+    methods: {
+        async fetchProducts() {
+            const products = await this.$axios.$get(this.$config.API_URL + 'products?populate=*')
+            this.products = products.data
+        }
+    },
+    mounted() {
+        this.fetchProducts()
+    }
+}
+
 </script>
